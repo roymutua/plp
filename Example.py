c14b6c16f9f192d4201c3e08bb880378a39b2f5f
roymutua <roy.ngure@gmail.com>
2026-02-12 17:56:55 +0300

    assignment 2

--- Example.py
+++ Example.py
@@ -1,8 +1,13 @@
-high_income = False
-good_credit= True
-student = False
+userSet1 = int(input("Enter the number of first set "))
 
-if high_income or good_credit and not student:
-    print("Eligible")
-else:
-    print("Not eligible")
+for i in range(userSet1):
+    number = int(input("Enter the set of integers "))
+
+userSet2 = int(input("Enter the number of second set "))
+for i in range(userSet2):
+    number = int(input("Enter the set of integers "))
+
+common = userSet1 & userSet2
+
+print(f'First set: {userSet1}\n Second set: {userSet2}')
+print(f'Common set: {common}')
--- plp.py
+++ plp.py
@@ -1,15 +1,61 @@
-num1 = 2
-print (num1 , "it is type" , type(num1))
 
-num2 = 3.4
-print(num2 , "it is type" , type(num2))
+#assignment week1
+value1 = input(" Input the first number")
+value2 = input(" Input the second number")
 
-num3 = "hello plp"
-print(num3 , "it is type" , type(num3))
+print(float(value1) + float(value2))
 
-num4 = 1+2j
-print(num4 , "it is type" , type(num4))
 
-name = input("What's your name? ")
-color = input("What's your favorite color? ")
-print ("Hello " +name, ", Your favourite color is, " + color)
+#assignment week 2
+# Question 1
+userInput = []
+
+user_input = int(input("How many values are you entering"))
+
+for i in range(userInput):
+    numbers = int(input("Enter the value"))
+
+total = sum(numbers)
+
+print(f'List of values:, {numbers}\n sum of values:, {total}')
+
+
+#Question 2
+Books = ("The Trap", "Revenge", "Betrayal", "Cold cases")
+
+for book in Books:
+    print(book)
+
+
+#Question 3
+personInformation = {}
+
+personInformation['Name'] = input("Your name ")
+personInformation['Age'] = int(input("Your age "))
+personInformation['Favourite colour'] = input("Your height ")
+
+print("information: ", personInformation)
+
+
+#Question 4
+userSet1 = int(input("Enter the number of first set "))
+
+for i in range(userSet1):
+    number = int(input("Enter the set of integers "))
+
+userSet2 = int(input("Enter the number of second set "))
+for i in range(userSet2):
+    number = int(input("Enter the set of integers "))
+
+common = userSet1 & userSet2
+
+print(f'First set: {userSet1}\n Second set: {userSet2}')
+print(f'Common set: {common}')
+
+
+#Question 5
+listOfWords = ['Apple', 'Chair', 'Tabletop', 'Mouse', 'Keyboard']
+
+oddWords = [ words for words in listOfWords if len(listOfWords) % 2 != 0 ]
+
+print(f'Original words: {listOfWords}\n Odd number words: {oddWords}')
